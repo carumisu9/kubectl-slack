8b7f083e0cdba7307d89ab2c6a60c8677dfa8ac2
Yuto Furuta <mzq6mft9zz@gmail.com>
2016-08-19 15:17:02 +0900

    Add k8s sniffer

--- src/main/main.go
+++ src/main/main.go
@@ -5,35 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
-
-	"github.com/joho/godotenv"
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/client/cache"
-	"k8s.io/kubernetes/pkg/client/restclient"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/client/unversioned/clientcmd"
 	"k8s.io/kubernetes/pkg/controller/framework"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/util/wait"
 )
 
-func envLoad() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
-
 func notifySlack(obj interface{}, action string) {
 
 	pod := obj.(*api.Pod)
-	//Incoming Webhook URL
-	url := os.Getenv("WEBHOOKURL")
-	//Form JSON payload to send to Slack
+	url := "https://hooks.slack.com/services/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"
 	json := `{"text": "Pod ` + action + ` in cluster: ` + pod.ObjectMeta.Name + `"}`
-	//Post JSON payload to the Webhook URL
 	client := http.Client{}
 	req, err := http.NewRequest("POST", url, bytes.NewBufferString(json))
 	req.Header.Set("Content-Type", "application/json")
@@ -50,11 +37,9 @@ func podDeleted(obj interface{}) {
 }
 func watchPods(client *client.Client, store cache.Store) cache.Store {
 
-	//Define what we want to look for (Pods)
 	watchlist := cache.NewListWatchFromClient(client, "pods", api.NamespaceAll, fields.Everything())
 	resyncPeriod := 30 * time.Minute
 
-	//Setup an informer to call functions when the watchlist changes
 	eStore, eController := framework.NewInformer(
 		watchlist,
 		&api.Pod{},
@@ -65,7 +50,6 @@ func watchPods(client *client.Client, store cache.Store) cache.Store {
 		},
 	)
 
-	//Run the controller as a goroutine
 	go eController.Run(wait.NeverStop)
 
 	return eStore
@@ -73,28 +57,19 @@ func watchPods(client *client.Client, store cache.Store) cache.Store {
 
 func main() {
 
-	envLoad()
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	loadingRules.ExplicitPath = clientcmd.RecommendedHomeFile
+	loader := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &clientcmd.ConfigOverrides{})
 
-	//Configure cluster info
-	config := &restclient.Config{
-		Host:     os.Getenv("SERVER_URL"),
-		Username: os.Getenv("USER"),
-		Password: os.Getenv("PASSWORD"),
-		Insecure: true,
-	}
+	clientConfig, err := loader.ClientConfig()
 
-	//Create a new client to interact with cluster and freak if it doesn't work
-	kubeClient, err := client.New(config)
+	kubeClient, err := client.New(clientConfig)
 	if err != nil {
 		log.Fatalln("Client not created sucessfully:", err)
 	}
 
-	//Create a cache to store Pods
 	var podsStore cache.Store
 
-	//Watch for Pods
 	podsStore = watchPods(kubeClient, podsStore)
 
-	//Keep alive
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
